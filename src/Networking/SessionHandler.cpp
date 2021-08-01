b7bce8ffe5b8890ac1246960bed379a7fcecdc19
Mateusz Walewski <mat.walewski@gmail.com>
2021-08-01 10:19:26 +0200

    nice!

--- src/Networking/SessionHandler.cpp
+++ src/Networking/SessionHandler.cpp
@@ -2,12 +2,19 @@
 #include "rpc/client.h"
 #include <memory>
 #include "SessionHandler.h"
+#include <iostream>
 
   SessionHandler::SessionHandler(std::shared_ptr<rpc::server> srv): mNodes(srv)
-  {};
+  {
+    std::cout << "SessionHandler ctor on the server side called ! " << std::endl;
+  
+  };
 
   SessionHandler::SessionHandler(std::shared_ptr<rpc::client> client): mHub(client)
-  {};
+  {
+    std::cout << "SessionHandler ctor on the client side called ! " << std::endl;
+  };
+
 
   std::shared_ptr<rpc::server> SessionHandler::GetNode() const
   {
