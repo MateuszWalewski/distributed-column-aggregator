e6d5995af43687ad91b307d7b372b02a4e2b2ebf
Mateusz Walewski <Mateusz.Walewski@brytlyt.com>
2021-08-03 16:14:28 +0200

    adding more todos

--- src/Aggregations/SimpleAggsDist.cpp
+++ src/Aggregations/SimpleAggsDist.cpp
@@ -10,6 +10,9 @@ namespace calcs
         // TODO: make session available here, for ex. from some singleton class storing SessionHandler
         // auto result = session.GetHub()->call("AllocateAndAggregate", containerSize, fillValue).as<int>();
 
+        // TODO: make the funtion making calls in parallel on many nodes 
+        // (simple parallelism in C++ with saving res to vector will be probably enough)
+
         return 5;
     }
 }  // namespace calcs
--- src/client.cpp
+++ src/client.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include "Networking/SessionHandler.h"
 #include "Aggregations/SimpleAggsDist.h"
+#include <string>
 
 int main() {
     SessionHandler session(std::make_shared<rpc::client>("127.0.0.1", 5555));
     int size = 6;
     int key = 4;
-    auto result = session.GetHub()->call("add", size, key).as<int>();
+    std::string methodName = "add";
+    auto result = session.GetHub()->call(methodName, size, key).as<int>();
     std::cout << "The result is: " << result << std::endl;
 
     auto result2 = calcs::AllocateAndAggregateDist(size, key);
