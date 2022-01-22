e835db2b50b2ba5f3eec8dd7c95387b45b70753f
Mateusz Walewski <mat.walewski@gmail.com>
2022-01-22 12:32:46 +0100

    Adding function preparing data to send on nodes

--- src/Tools/Utility.h
+++ src/Tools/Utility.h
@@ -10,3 +10,26 @@ template <typename T> void PrintVector( std::vector<T> container, const std::str
         std::cout << label << ": " << x << std::endl;
     }
 }
+
+// make it cleaner
+template <typename T> std::vector<std::vector<T>> PartitionDataToSendOnNodes( const std::vector<T> & vec, size_t n )
+{
+    std::vector<std::vector<T>> outVec;
+
+    size_t length = vec.size() / n;
+    size_t remain = vec.size() % n;
+
+    size_t begin = 0;
+    size_t end = 0;
+
+    for ( size_t i = 0; i < std::min( n, vec.size() ); ++i )
+    {
+        end += ( remain > 0 ) ? ( length + !!( remain-- ) ) : length;
+
+        outVec.push_back( std::vector<T>( vec.begin() + begin, vec.begin() + end ) );
+
+        begin = end;
+    }
+
+    return outVec;
+}
