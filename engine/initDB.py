48418bf4f551eb26b00293c939a745990f5c05ed
MateuszWalewski <32540764+MateuszWalewski@users.noreply.github.com>
2024-05-16 16:13:35 +0200

    Making the remote nodes working back again (#2)

--- engine/initDB.py
+++ engine/initDB.py
@@ -2,10 +2,10 @@ import os
 import sys
 sys.path.append('/usr/local/lib')
 
-from interpreter import LoadDependencies
+from interpreter import InitialiseDB
 from interpreter import DoubleColumn
 from interpreter import FloatColumn
 from interpreter import IntColumn
 
-LoadDependencies(os.environ['CONN_CONF'])
+InitialiseDB()
 
