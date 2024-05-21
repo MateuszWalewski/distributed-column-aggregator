f748d23d0e7a1850644ed6f064b43cd4a5b8deb2
Mateusz Walewski <hraboa77@gmail.com>
2024-05-21 14:46:21 +0200

    Changing the repo name to be more descriptive

--- scripts/run_benchmarks_2_nodes.sh
+++ scripts/run_benchmarks_2_nodes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker compose -f $HOME/distributedDBPrototype/docker_setup/1_hub_2_nodes.yml down -t0
-docker compose -f $HOME/distributedDBPrototype/docker_setup/1_hub_2_nodes.yml up -d
+docker compose -f $HOME/distributed-column-aggregator/docker_setup/1_hub_2_nodes.yml down -t0
+docker compose -f $HOME/distributed-column-aggregator/docker_setup/1_hub_2_nodes.yml up -d
 
 docker exec -it hub bash -c "pytest"
-docker compose -f $HOME/distributedDBPrototype/docker_setup/1_hub_2_nodes.yml down -t0
+docker compose -f $HOME/distributed-column-aggregator/docker_setup/1_hub_2_nodes.yml down -t0
--- scripts/run_ctest.sh
+++ scripts/run_ctest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker compose -f $HOME/distributedDBPrototype/docker_setup/1_hub.yml down -t0
-docker compose -f $HOME/distributedDBPrototype/docker_setup/1_hub.yml up -d
+docker compose -f $HOME/distributed-column-aggregator/docker_setup/1_hub.yml down -t0
+docker compose -f $HOME/distributed-column-aggregator/docker_setup/1_hub.yml up -d
 
 docker exec -it hub bash -c "cd engine/build/ && ctest -V"
-docker compose -f $HOME/distributedDBPrototype/docker_setup/1_hub.yml down -t0
+docker compose -f $HOME/distributed-column-aggregator/docker_setup/1_hub.yml down -t0
